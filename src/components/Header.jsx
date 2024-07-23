5a1fa7ba9445f55080e4d308d13f55cf1ff595a2
Anand Verma <anandverma552@gmail.com>
2024-07-23 10:29:37 -0400

    Adding base components

---
+++ src/components/Header.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { Button } from "./ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+
+const Header = () => {
+  const navigate = useNavigate();
+  const user = false;
+  return (
+    <nav className="py-4 flex justify-between items-center">
+      <Link to="/">
+        <img src="logo.png" className="h-24" alt="logo" />
+      </Link>
+      <div>
+        {!user ? (
+          <DropdownMenu>
+            <DropdownMenuTrigger>
+              <Avatar>
+                <AvatarImage src="https://github.com/shadcn.png" />
+                <AvatarFallback>CN</AvatarFallback>
+              </Avatar>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuLabel>My Account</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>Profile</DropdownMenuItem>
+              <DropdownMenuItem>Billing</DropdownMenuItem>
+              <DropdownMenuItem>Team</DropdownMenuItem>
+              <DropdownMenuItem>Subscription</DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        ) : (
+          <Button onClick={() => navigate("/auth")}>Login</Button>
+        )}
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
---
+++ src/layouts/app-layout.jsx
@@ -0,0 +1,23 @@
+import Header from "@/components/Header";
+import React from "react";
+import { Outlet } from "react-router-dom";
+
+const AppLayout = () => {
+  return (
+    <div>
+      <main className="min-h-screen container">
+       <Header />
+        { <Outlet /> }
+      </main>
+     <div>
+        <footer className="py-4 text-center bg-gray-500 text-sm">
+           
+            <p>Made with Love by Anand Verma </p>
+            
+        </footer>
+     </div>
+    </div>
+  );
+};
+
+export default AppLayout;
